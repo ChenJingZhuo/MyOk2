714a7ce923851e4ad9f2498ae4cb8e52c95231f1
ChenJingZhuo <3279838729@qq.com>
2020-04-06 00:03:27 +0800

    1.3

--- app/src/main/java/com/cjz/myok/MyOk2.java
+++ app/src/main/java/com/cjz/myok/MyOk2.java
@@ -12,29 +12,32 @@ import okhttp3.Response;
 
 public abstract class MyOk2 {
 
-    public MyOk2(final String uri, String args){
-        final OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+    private static final OkHttpClient client = new OkHttpClient().newBuilder().build();
+    private static final MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+
+    public MyOk2(final String uri, String args) {
         final RequestBody body = RequestBody.create(mediaType, args);
-        new Thread(){
-            Request request = new Request.Builder()
-                    .url("http://192.168.1.112:8085/"+uri)
-                    .method("POST", body)
-                    .addHeader("Content-Type", "application/x-www-form-urlencoded")
-                    .build();
-            try {
+        new Thread() {
+            @Override
+            public void run() {
+                Request request = new Request.Builder()
+                        .url("http://192.168.1.112:8085/" + uri)
+                        .method("POST", body)
+                        .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                        .build();
+                try {
                     Response response = client.newCall(request).execute();
                     JSONObject jsonObject = new JSONObject(response.body().string());
-                    if (jsonObject.getString("status").equals("200")){
+                    if (jsonObject.getString("status").equals("200")) {
                         ok(jsonObject);
-                    }else {
+                    } else {
                         ok(null);
                         Log.d("error", jsonObject.toString());
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+            }
         }.start();
     }
 
