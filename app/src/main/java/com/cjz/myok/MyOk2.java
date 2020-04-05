826ee2f0d9dabc985e02255b1e10cea687543ca8
ChenJingZhuo <3279838729@qq.com>
2020-04-05 23:20:54 +0800

    1.2.1

--- app/src/main/java/com/cjz/myok/MyOk2.java
+++ app/src/main/java/com/cjz/myok/MyOk2.java
@@ -12,28 +12,30 @@ import okhttp3.Response;
 
 public abstract class MyOk2 {
 
-    public MyOk2(String uri, String args){
-        OkHttpClient client = new OkHttpClient().newBuilder()
+    public MyOk2(final String uri, String args){
+        final OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
-        RequestBody body = RequestBody.create(mediaType, args);
-        Request request = new Request.Builder()
-                .url("http://192.168.1.112:8085/"+uri)
-                .method("POST", body)
-                .addHeader("Content-Type", "application/x-www-form-urlencoded")
-                .build();
-        try {
-            Response response = client.newCall(request).execute();
-            JSONObject jsonObject = new JSONObject(response.body().string());
-            if (jsonObject.getString("status").equals("200")){
-                ok(jsonObject);
-            }else {
-                ok(null);
-                Log.d("error", jsonObject.toString());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        final RequestBody body = RequestBody.create(mediaType, args);
+        new Thread(){
+            Request request = new Request.Builder()
+                    .url("http://192.168.1.112:8085/"+uri)
+                    .method("POST", body)
+                    .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                    .build();
+            try {
+                    Response response = client.newCall(request).execute();
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                    if (jsonObject.getString("status").equals("200")){
+                        ok(jsonObject);
+                    }else {
+                        ok(null);
+                        Log.d("error", jsonObject.toString());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+        }.start();
     }
 
     public abstract void ok(JSONObject jsonObject);
